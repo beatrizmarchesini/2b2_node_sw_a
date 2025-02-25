4454f8c114127615215978f4118ab323216ebf36
beatrizmarchesini <beatriz.marchesini@etec.sp.gov.br>
2025-02-25 18:17:35 -0300

    funções node

---
+++ aula2/index.js
@@ -0,0 +1,20 @@
+function soma(n1, n2){
+        return (n1 + n2)
+}
+
+function sub(n1, n2){
+    return (n1 - n2)
+}
+
+function mult(n1, n2){
+    return (n1 * n2)
+}
+
+function div(n1, n2){
+    return (n1 / n2)
+}
+
+console.log ((13, 63))
+
+
+
